0c55c68be3d7e68055182cc944f03ad3d15f99bb
Joshua Cilufo <joshua.cilufo@student.swic.edu>
2025-09-27 20:05:24 -0500

    Arrange functions in alphabetical order

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,31 @@
 import COURSES from "./data.js";
 export { getStudentPercentage, getClassAverage, addAssignment };
 
+const addAssignment = ({ courseId, assignmentName, maxPoints }) => {
+  const clonedCourses = structuredClone(COURSES);
+  const foundCourse = clonedCourses.find(({ id }) => id === courseId);
+
+  const newAssignment = { name: assignmentName, points: null, maxPoints };
+
+  foundCourse.students = foundCourse.students.map((student) => ({
+    ...student,
+    assignments: [...student.assignments, newAssignment],
+  }));
+
+  return clonedCourses;
+};
+
+const getClassAverage = (courseId) => {
+  const foundCourse = COURSES.find(({ id }) => id === courseId);
+  const totalStudents = foundCourse.students.length;
+
+  return Math.round(
+    foundCourse.students
+      .map(({ id }) => getStudentPercentage(courseId, id))
+      ?.reduce((acc, percentage) => acc + percentage, 0) / totalStudents
+  );
+};
+
 const getStudentPercentage = (courseId, studentId) => {
   const { totalPoints, totalMaxPoints } = COURSES.find(
     ({ id }) => id === courseId
@@ -15,29 +40,4 @@ const getStudentPercentage = (courseId, studentId) => {
     ) || { totalPoints: 0, totalMaxPoints: 0 };
 
   return Math.round((totalPoints / totalMaxPoints) * 100);
-};
-
-const getClassAverage = (courseId) => {
-  const foundCourse = COURSES.find(({ id }) => id === courseId);
-  const totalStudents = foundCourse.students.length;
-
-  return Math.round(
-    foundCourse.students
-      .map(({ id }) => getStudentPercentage(courseId, id))
-      ?.reduce((acc, percentage) => acc + percentage, 0) / totalStudents
-  );
-};
-
-const addAssignment = ({ courseId, assignmentName, maxPoints }) => {
-  const clonedCourses = structuredClone(COURSES);
-  const foundCourse = clonedCourses.find(({ id }) => id === courseId);
-
-  const newAssignment = { name: assignmentName, points: null, maxPoints };
-
-  foundCourse.students = foundCourse.students.map((student) => ({
-    ...student,
-    assignments: [...student.assignments, newAssignment],
-  }));
-
-  return clonedCourses;
 };
